e210f40d88bc810d1ff0be0fb1ef5d0b5e6fcd52
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-05-23 18:09:52 -0300

    adding initial files + first route

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router, Request, Response } from "express";
+import { v4 as uuidV4 } from "uuid";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/", (req: Request, res: Response) => {
+  const { name, description } = req.body;
+
+  const category = {
+    name,
+    description,
+    id: uuidV4(),
+  };
+
+  categories.push(category);
+
+  return res.status(201).send();
+});
+
+export { categoriesRoutes };
